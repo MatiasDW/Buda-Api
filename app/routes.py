ed6472debc07ae8daed7b8e6eb502f1ee167b618
Matias Davila <matiasdw8@gmail.com>
2023-12-28 01:08:10 -0300

    Add every market to the functions

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,15 @@
 import requests
-from flask import jsonify, request
+from flask import jsonify, request, Response
 from utils import calculate_spread as calculate_spread_util, set_alert as set_alert_util, check_alert as check_alert_util
+
+def get_markets():
+    response = requests.get('https://www.buda.com/api/v2/markets')
+    markets = response.json()
+
+    for market in markets['markets']:
+        print(market['id'])
+
+get_markets()
 
 def define_routes(app):
 
@@ -19,16 +28,19 @@ def define_routes(app):
     
     @app.route('/')
     def home():
-        market_id = "btc-clp"  # real market ID
-        spread_info, status_code = calculate_spread(market_id)  # Unpack the tuple
+        response = requests.get('https://www.buda.com/api/v2/markets')
+        markets = response.json()
+        spreads = []
 
-        #Chek the status code to extract data.
-        if status_code == 200:
-            spread_data = spread_info.json  # Access to .json metod to obtain data.
-            return f"El spread para el mercado {market_id} es {spread_data['spread']} CLP"
-        else:
-            return "No se pudo obtener el spread para el mercado especificado."
+        for market in markets['markets']:
+            market_id = market['id']
+            spread_info, status_code = calculate_spread(market_id)
+            if status_code == 200:
+                spread_data = spread_info.json
+                spreads.append(f"El spread para el mercado {market_id} es {spread_data['spread']} CLP")
+            else:
+                spreads.append(f"No se pudo obtener el spread para el mercado {market_id}")
 
-
+        return Response("<br>".join(spreads), mimetype='text/html')
 
   
--- app/utils.py
+++ app/utils.py
@@ -15,12 +15,14 @@ def calculate_spread(market_id):
 
     if response.status_code == 200:
         order_book = response.json()['order_book']
-        response = requests.get(order_book_url)
-        best_bid = float(order_book['bids'][0][0])  # Highest buy order
-        best_ask = float(order_book['asks'][0][0])  # Lowest sell order
-        spread = best_ask - best_bid  # Calculate spread
-        print(f'Spread for market {market_id}: {spread}')
-        return {'market_id': market_id, 'spread': spread}, 200
+        if order_book['bids'] and order_book['asks']:  # Check if 'bids' and 'asks' lists are not empty
+            best_bid = float(order_book['bids'][0][0])  # Highest buy order
+            best_ask = float(order_book['asks'][0][0])  # Lowest sell order
+            spread = best_ask - best_bid  # Calculate spread
+            print(f'Spread for market {market_id}: {spread}')
+            return {'market_id': market_id, 'spread': spread}, 200
+        else:
+            return {'error': f'No bids or asks for market {market_id}'}, 400
     else:
         return {'error': 'Unable to fetch order book'}, response.status_code
 
